89e3349cb155d6967eea08c98335a10890c19ff0
Max de Vocht <maxdevocht@hotmail.com>
2024-06-12 12:04:38 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 // import { useLocation } from "react-router-dom";
 import { webcam } from "../assets";
-import { navigation } from "../constants";
+// import { navigation } from "../constants";
 // import Button from "./Button";
 // import { useState } from "react";
 
@@ -34,17 +34,12 @@ const Header = () => {
 
         <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 lg:static lg:flex lg:mx-auto">
           <div className="relative z-2 flex flex-col lg:flex-row items-center justify-center m-auto lg:flex-grow">
-            {navigation.map((item) => (
-              <a
-                key={item.id}
-                href={item.url}
-                className="block relative font-code text-2xl uppercase"
-              >
-                {item.title}
-              </a>
-            ))}
+            <a href="#about">About</a>
+            <a href="#design">Design</a>
+            <a href="#pricing">Pricing</a>
           </div>
         </nav>
+        <a href="#contact">Contact</a>
       </div>
     </div>
   );
